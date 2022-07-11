2a8b175c4ce88f0c765d7d123a40d354891078fe
Wendy You <wyou130@gmail.com>
2022-07-11 10:12:18 -0400

    update js script with tabs

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function openTab(tabName, elmt, color) {
+    var i, headerTab, tablinks;
+    headerTab = document.getElementsByClassName("headerTab");
+    for (i = 0; i < headerTab.length; i++) {
+        headerTab[i].style.display = "none"
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    elmt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click()
